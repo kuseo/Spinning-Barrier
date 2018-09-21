d3faac19109fd2e5d32d81f85460a494cd9013c4
kanguk Seo <tjrkddnr@korea.ac.kr>
2018-09-22 02:14:57 +0900

    implement funciton drawBarrier : draw a segment of circle

---
+++ Spinning Barrier/Drawing.h
@@ -0,0 +1,24 @@
+#ifndef __DRAWING_H__
+#define __DRAWING_H__
+
+#include "gl_helper.h"
+#define radian(degree) ((degree)*(3.141592)/180.0f)
+
+void drawBarrier(float radius, float angle)
+{
+	float temp = radius;
+	float x, y;
+
+	glPointSize(3.0);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glBegin(GL_POINTS);
+	for (int degree = 0; degree <= (angle*2); degree++)
+	{
+		x = radius * cos(radian(degree/2));
+		y = radius * sin(radian(degree/2));
+		glVertex2f(x, y);
+	}
+	glEnd();
+}
+
+#endif // ! __DRAWING_H__
