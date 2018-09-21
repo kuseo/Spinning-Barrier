2b7c8830a761a2d1ea3ba7b78f6f84d6eb0020fe
kanguk Seo <tjrkddnr@korea.ac.kr>
2018-09-22 04:13:42 +0900

    implement function drawEnemy : draw simple triangle

--- Spinning Barrier/Drawing.h
+++ Spinning Barrier/Drawing.h
@@ -20,4 +20,29 @@ void drawBarrier(float radius, int angle)
 	glEnd();
 }
 
+void drawEnemy()
+{
+	float w = 1/(float)glutGet(GLUT_WINDOW_WIDTH);
+	float h = 1/(float)glutGet(GLUT_WINDOW_HEIGHT);
+
+	glPointSize(1.0);
+	glBegin(GL_POINTS);
+	for (int i = 0; i <= 20; i++)
+	{
+		float y = (float)i * h;
+		glVertex2f(0.0, y);
+		for (int j = i; j != 0; j--)
+		{
+			glVertex2f((float)j*w, y);
+			glVertex2f((-1)*(float)j*w, y);
+		}
+	}
+	glEnd();
+}
+
+void drawPrincess()
+{
+
+}
+
 #endif // ! __DRAWING_H__
