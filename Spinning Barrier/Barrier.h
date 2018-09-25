86334a8ee3136c60f1e1ca7691c1e62ef82f8b5f
서강욱 <tjrkddnr@korea.ac.kr>
2018-09-26 03:01:33 +0900

    encapsulation

--- Spinning Barrier/Barrier.h
+++ Spinning Barrier/Barrier.h
@@ -1,6 +1,8 @@
 #ifndef __BARRIER_H__
 #define __BARRIER_H__
 
+#include "Enemy.h"
+
 class Barrier
 {
 public:
@@ -22,7 +24,21 @@ public:
 	member function
 	*/
 	void updateRotate(float deltaTime) { Rotate += deltaTime * Speed; }
+	bool enemyCollide(Enemy enemy)
+	{
+		/*
+		casting float type into int to handle the precision error
+		*/
+		int rotate_begin = (int)Rotate % 360;
+		if (enemy.Rotate >= AngleBetween &&  rotate_begin < 0)
+			rotate_begin += 360;
+		int rotate_end = rotate_begin + (int)AngleBetween;
 
+		if (!enemy.collide && 25 < (int)(enemy.Distance * 100) && (int)(enemy.Distance * 100) < 30
+			&& rotate_begin < (int)enemy.Rotate && int(enemy.Rotate) < rotate_end)
+			return true;
+		return false;
+	}
 
 };
 #endif // !__BARRIER_H__
